94253d05d7fdbaab338e4f610635555e307c396f
David StClair <davidstclair@laybuy.com>
2022-12-05 19:22:20 +0000

    adding simple test for ipnet pkg

--- pkg/ipnet/ipnet_test.go
+++ pkg/ipnet/ipnet_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"reflect"
 	"testing"
 
 	"gopkg.in/netaddr.v1"
@@ -25,18 +24,18 @@ func TestIPv4toDecimal(t *testing.T) {
 }
 
 func TestProcessCidr(t *testing.T) {
+	//This test checks for values on custom "cidr" struct
 
+	//define the data we are looking for
 	cidrString := "82.12.162.0/24"
 
 	_, ipnet, err := net.ParseCIDR(cidrString)
 
 	netip := netaddr.NetworkAddr(ipnet)
-	// bcastip := netaddr.BroadcastAddr(ipnet)
+	bcastip := netaddr.BroadcastAddr(ipnet)
 
+	//get the output
 	cidr, err := ProcessCidr(cidrString)
-
-	fmt.Println(reflect.TypeOf(cidr.BcastIP))
-	fmt.Println(reflect.TypeOf(netip))
 
 	if err != nil {
 		if err != nil {
@@ -44,9 +43,16 @@ func TestProcessCidr(t *testing.T) {
 		}
 	}
 
-	got := cidr.BcastIP
-
-	if got != netip {
-
+	gotbcastIP := fmt.Sprintf("%s", cidr.BcastIP)
+	wantbcastIP := fmt.Sprintf("%s", bcastip)
+	if gotbcastIP != wantbcastIP {
+		t.Errorf("Expected '%s', but got '%s'", wantbcastIP, gotbcastIP)
 	}
+
+	gotnetip := fmt.Sprintf("%s", cidr.NetIP)
+	wantnetip := fmt.Sprintf("%s", netip)
+	if gotnetip != wantnetip {
+		t.Errorf("Expected '%s', but got '%s'", wantnetip, gotnetip)
+	}
+
 }
